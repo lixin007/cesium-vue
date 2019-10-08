75dfa8a6d237b1607169dfb88faf7f009484ca51
lx007 <122410876@qq.com>
2019-10-08 09:45:20 +0800

    Signed-off-by: lx007 <122410876@qq.com>

--- src/views/test/test0006.vue
+++ src/views/test/test0006.vue
@@ -3,9 +3,10 @@
       <span v-aa1>{{message}}</span>
 <!--      <my-try colorType="a-danger font-big"/>-->
       <test-toast ref="toast"></test-toast>
-      <jed-button type="danger">hansu</jed-button><br>
+      <jed-button type="danger" ref="a1"  @click.native="aa">hansu9</jed-button><br>
       <jed-button type="warning">hansu</jed-button><br>
       <jed-button type="danger" size="large">hansu</jed-button>
+      {{color3}} <br/> {{message}}<br/> {{a1}}
     </div>
 </template>
 
@@ -24,16 +25,46 @@
         color3:"red"
       }
     },
+    methods: {
+      aa() {
+        // this.color3 ="blue"
+        // alert(this.color3)
+        const a = [{id:1,age:10},{id:2,age:20},{id:3,age:30}]
+        const b = [{id:3,sex:'男'},{id:1,sex:'男'},{id:2,sex:'女'}]
+        let c = [];
+        for(let u of a) {
+          const temp = b.filter(i => i.id === u.id)[0];
+          c.push({...u, ...temp});
+        }
+        console.log(c)
+      },
+      aa2(){
+        alert("update")
+      }
+    },
+    /*updated(){
+      debugger
+      this.aa2()
+    },*/
+
     mounted() {
       this.$myMethod();
+
       Vue.myGlobalMethod();
-      this.formatDate("56")
+      // this.formatDate("56")
+      console.log(this.$refs)
       // tsg.npmTest("84710")
       // tsg.yz()
       // const { yz2,yz4 } = tsg
       // console.log(yz2)
       // const person = new yz4('leon',30)
       // console.log(person.say())
+    },
+    computed: {
+      a1:function(){
+        return this.color3+"好"
+      },
+
     }
   }
 </script>
